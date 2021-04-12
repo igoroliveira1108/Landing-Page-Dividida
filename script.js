10859e31420433ab9ed8ad7bfda4eaa29fe46ec2
igoroliveira1108 <igoroliveira110897@gmail.com>
2021-04-12 20:28:55 -0300

    Commit Inicial

---
+++ script.js
@@ -0,0 +1,13 @@
+// Variables
+const left = document.querySelector('.left');
+const right = document.querySelector('.right');
+const container = document.querySelector(".container");
+
+
+// Left Side
+left.addEventListener('mouseenter', () => container.classList.add('hover-left'));
+left.addEventListener('mouseleave', () => container.classList.remove('hover-left'));
+
+// Right Side
+right.addEventListener('mouseenter', () => container.classList.add('hover-right'));
+right.addEventListener('mouseleave', () => container.classList.remove('hover-right'));
